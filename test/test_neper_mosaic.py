c25e47dc9045ee734741acf46b14823d5724ffcb
Tim Furlan <tim.furlan@tu-dortmund.de>
2024-09-09 11:32:11 +0200

    Mesh should always start at (0,0,0)

--- test/test_neper_mosaic.py
+++ test/test_neper_mosaic.py
@@ -21,11 +21,11 @@ def test_main_2d(tmp_path):
     output_files = [tmp_path / "out.geo_unrolled",
                     tmp_path / "out.msh",
                     ]
-    
+
     main(input_file, output_files)
-    
+
     for file in output_files:
-	    assert file.is_file()
+        assert file.is_file()
 
 def test_main_3d(tmp_path):
     """The Mosaic main() routine works for the 3d case."""
@@ -35,97 +35,97 @@ def test_main_3d(tmp_path):
                     ]
 
     main(input_file, output_files)
-    
+
     for file in output_files:
-	    assert file.is_file()
-	    
+        assert file.is_file()
+
 def test_domain_position_2d(tmp_path):
     """Mosaic still works in 2d with a change in the domain."""
     input_file = "2d-n5-id1.geo"
     output_files = [tmp_path / "out.msh"]
-    
+
     main(input_file,
          output_files,
          box_x_position=0.1,
          box_y_position=0.1)
-    
+
     for file in output_files:
-	    assert file.is_file()
-	    
+        assert file.is_file()
+
 def test_exception_domain_position_wrong_2d(tmp_path):
     """
     Domain position change raises exception when volume is incomplete in 2d.
-    
+
     """
     input_file = "2d-n5-id1.geo"
     output_files = [tmp_path / "out.msh"]
-    
+
     with pytest.raises(RuntimeError) as e:
-	    main(input_file,
-	         output_files,
-	         box_x_position=0.8,
-	         box_y_position=0.8)
-    
+        main(input_file,
+             output_files,
+             box_x_position=0.8,
+             box_y_position=0.8)
+
     assert e.match("Model volume after trimming is wrong.")
-    
+
 def test_domain_position_3d(tmp_path):
     """Mosaic still works in 3d with a change in the domain."""
     input_file = "3d-n4-id1.geo"
     output_files = [tmp_path / "out.msh"]
-    
+
     main(input_file,
          output_files,
          box_x_position=0.1,
          box_y_position=0.1)
-    
+
     for file in output_files:
-	    assert file.is_file()
-	    
+        assert file.is_file()
+
 def test_exception_domain_position_wrong_3d(tmp_path):
     """
     Domain position change raises exception when volume is incomplete in 3d.
-    
+
     """
     input_file = "3d-n4-id1.geo"
     output_files = [tmp_path / "out.msh"]
-    
+
     with pytest.raises(RuntimeError) as e:
-	    main(input_file,
-	         output_files,
-	         box_x_position=0.8,
-	         box_y_position=0.8)
-    
+        main(input_file,
+             output_files,
+             box_x_position=0.8,
+             box_y_position=0.8)
+
     assert e.match("Model volume after trimming is wrong.")
-    
+
 def test_ciGen(tmp_path):
     """The ciGen option does not break Mosaic (ciGen is not tested)."""
     input_file = "2d-n5-id1.geo"
     output_files = [tmp_path / "out.msh"]
-    
+
     main(input_file, output_files, ciGen=True)
-    
+
     for file in output_files:
-	    assert file.is_file()
-	    
+        assert file.is_file()
+
 def test_loading_2d(gmsh_instance, tmp_path):
-	"""load_rectilinear_geometry() is working for 2d."""
+    """load_rectilinear_geometry() is working for 2d."""
     input_file = "2d-n5-id1.geo"
-    
+
     assert len(gmsh.model.getEntities()) == 0
-    
+
     load_rectilinear_geometry(input_file)
     assert len(gmsh.model.getEntities()) > 0
-    
+
 def test_loading_3d(gmsh_instance, tmp_path):
-	"""load_rectilinear_geometry() is working in 3d."""
+    """load_rectilinear_geometry() is working in 3d."""
     input_file = "3d-n4-id1.geo"
-    
+
     assert len(gmsh.model.getEntities()) == 0
-    
+
     load_rectilinear_geometry(input_file)
     assert len(gmsh.model.getEntities()) > 0
 
 if __name__ == "__main__":
     pytest.main()
-    
-    
+
+
